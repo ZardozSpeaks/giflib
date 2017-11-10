d859062c390410c9121f8872c79efccec26527cf
David Remington <david.ian.remington@gmail.com>
2017-11-09 16:08:17 -0800

    update templates and resources

--- src/main/java/com/davidremington/giflib/controller/GifController.java
+++ src/main/java/com/davidremington/giflib/controller/GifController.java
@@ -1,15 +1,34 @@
 package com.davidremington.giflib.controller;
 
+import com.davidremington.giflib.data.GifRepository;
+import com.davidremington.giflib.model.Gif;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class GifController {
 
-    @RequestMapping(value = "/")
-    @ResponseBody
-    public String listGifs(){
-        return "List of all the gifs";
+    private GifRepository gifRepository;
+
+    @Autowired
+    public GifController(GifRepository gifRepository) {
+        this.gifRepository = gifRepository;
     }
+
+    @RequestMapping(value = "/")
+    public String listGifs(){
+        return "home";
+    }
+
+    @RequestMapping("/gif/{name}")
+    public String gifDetails(@PathVariable String name, ModelMap modelMap) {
+        Gif gif = gifRepository.findByName(name);
+        modelMap.put("gif", gif);
+        return "gif-details";
+    }
+
+
 }
