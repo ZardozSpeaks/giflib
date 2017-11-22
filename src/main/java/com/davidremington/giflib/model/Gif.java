d595bc2ebbfcef106bcdc3c6033f227c90bbf085
David Remington <david.ian.remington@gmail.com>
2017-11-22 12:50:34 -0800

    update Gif to Entity

--- src/main/java/com/davidremington/giflib/model/Gif.java
+++ src/main/java/com/davidremington/giflib/model/Gif.java
@@ -1,59 +1,109 @@
 package com.davidremington.giflib.model;
 
-import java.time.LocalDate;
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 
+@Entity
 public class Gif {
-    private String name;
-    private int catergoryId;
-    private LocalDate dateUploaded;
-    private String username;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Lob
+    private byte[] bytes;
+    private String description;
+
+    @ManyToOne
+    private Category category;
+    private LocalDateTime dateUploaded = LocalDateTime.now();
+    private String username = "You";
     private boolean favorite;
+    private String hash;
 
-    public Gif(String name, int catergoryId, LocalDate dateUploaded, String username, boolean favorite) {
-        this.name = name;
-        this.catergoryId = catergoryId;
-        this.dateUploaded = dateUploaded;
-        this.username = username;
-        this.favorite = favorite;
+    public Gif() {}
+
+   public String getTimeSinceUploaded() {
+        String unit = "";
+        LocalDateTime now = LocalDateTime.now();
+        long diff;
+       if((diff = ChronoUnit.SECONDS.between(dateUploaded, now)) < 60){
+           unit = "secs";
+       } else if ((diff = ChronoUnit.MINUTES.between(dateUploaded, now)) < 60) {
+           unit = "mins";
+       } else if ((diff = ChronoUnit.HOURS.between(dateUploaded, now)) < 24) {
+           unit = "hours";
+       } else if ((diff = ChronoUnit.DAYS.between(dateUploaded, now)) < 30) {
+           unit = "days";
+       } else if ((diff = ChronoUnit.MONTHS.between(dateUploaded, now)) < 12) {
+           unit = "months";
+       } else{
+           diff = ChronoUnit.YEARS.between(dateUploaded, now);
+       }
+       return String.format("%d %s",diff,unit);
+   }
+
+    public Long getId() {
+        return id;
     }
 
-    public int getCatergoryId() {
-        return catergoryId;
+    public void setId(Long id) {
+        this.id = id;
     }
 
-    public void setCatergoryId(int catergoryId) {
-        this.catergoryId = catergoryId;
+    public byte[] getBytes() {
+        return bytes;
     }
 
-    public String getName() {
-        return name;
+    public void setBytes(byte[] bytes) {
+        this.bytes = bytes;
     }
 
-    public LocalDate getDateUploaded() {
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public LocalDateTime getDateUploaded() {
         return dateUploaded;
+    }
+
+    public void setDateUploaded(LocalDateTime dateUploaded) {
+        this.dateUploaded = dateUploaded;
     }
 
     public String getUsername() {
         return username;
     }
 
-    public boolean isFavorite() {
-        return favorite;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setDateUploaded(LocalDate dateUploaded) {
-        this.dateUploaded = dateUploaded;
-    }
-
     public void setUsername(String username) {
         this.username = username;
     }
 
+    public boolean isFavorite() {
+        return favorite;
+    }
+
     public void setFavorite(boolean favorite) {
         this.favorite = favorite;
+    }
+
+    public String getHash() {
+        return hash;
+    }
+
+    public void setHash(String hash) {
+        this.hash = hash;
     }
 }
